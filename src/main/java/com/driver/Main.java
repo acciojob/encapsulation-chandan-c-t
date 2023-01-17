acad76a457f14a37a95c6f71d1b7a7323d602c33
chandan-c-t <chandantalavar777@gmail.com>
2023-01-17 13:23:43 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+       // obj.name = "kunal";
+
+        obj.setName("Kunal");
+
+        obj.getName();
+    }
   
 }
